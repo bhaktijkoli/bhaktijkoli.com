b649ea7273acd1c81b50453626644fb615e7d3fd
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-01-25 10:31:02 +0530

    chore: added calendly

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import Header from "../components/Common/Header/Header";
 import Layout from "../components/Layout/Layout/Layout";
 import type { NextPage } from "next";
+import Script from "next/script";
 
 const schedule: NextPage = () => {
   return (
     <Layout>
       <React.Fragment>
-        <Header title="Schedule your Appointment" />
-        <iframe
-          width="100%"
-          height="750px"
-          src="https://bhaktijkoli-bhaktijkoli.zohobookings.com/portal-embed#/customer/4310576000000027015"
-          frameBorder={0}
-          allowFullScreen
-        >
-          {" "}
-        </iframe>
+        <Header title="Schedule a Meet" />
+        <div
+          className="calendly-inline-widget"
+          data-url="https://calendly.com/bhaktijkoli/meet?primary_color=5a40ca"
+          style={{
+            minWidth: 320,
+            height: 630,
+          }}
+        ></div>
+        <Script src="https://assets.calendly.com/assets/external/widget.js" />
       </React.Fragment>
     </Layout>
   );
