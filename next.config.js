fdf76fba4b120fc2efd3cdf1aeee1d38b4fdacec
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-11-21 10:13:50 +0530

    feat: added pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
